fbf1345071c4941c85062c4229f12cb346519404
Emily Loggins <em.loggins7@gmail.com>
2019-05-20 14:32:37 -0500

    added nav bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
 import './index.css';
 
 
 import Kennel from './components/Kennel'
 
-ReactDOM.render(<Kennel />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Kennel />
+    </Router>
+    , document.getElementById('root'));
 
 
